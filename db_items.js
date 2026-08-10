180fdc837842c004b9eaab2fd268adafcb9d3093
Kanna Dallas <121131284+KannaDallas@users.noreply.github.com>
2026-08-10 18:07:05 +0300

    Add files via upload

---
+++ db_items.js
@@ -0,0 +1,10 @@
+const ItemsDatabase = {
+    "Зелье маны": {
+        weight: 0.5, type: "consumable",
+        description: "Мгновенно восстанавливает 10 ОМ."
+    },
+    "Стальной фолиант": {
+        weight: 1.5, type: "grimoire",
+        description: "Дает +2 дополнительных слота заклинаний в Гримуаре."
+    }
+};
