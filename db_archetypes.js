c175a6276c2b8687e58d1512814ad847931d46e2
Kanna Dallas <121131284+KannaDallas@users.noreply.github.com>
2026-08-10 23:23:47 +0300

    Add files via upload

---
+++ db_archetypes.js
@@ -0,0 +1,8 @@
+const ArchetypesDatabase = {
+    "Паладин Света": { str: 14, dex: 10, int: 14, per: 10, end: 12, cha: 10, sch1: "Свет" },
+    "Жрец Света": { str: 10, dex: 10, int: 16, per: 12, end: 12, cha: 10, sch1: "Свет" },
+    "Ассасин": { str: 10, dex: 16, int: 10, per: 14, end: 10, cha: 10, sch1: "Тень" },
+    "Механист (Инженер)": { str: 10, dex: 10, int: 16, per: 14, end: 10, cha: 10, sch1: "Инженерия" },
+    "Владыка Костей": { str: 10, dex: 10, int: 16, per: 12, end: 10, cha: 12, sch1: "Некромантия" },
+    "Культист Хаоса": { str: 10, dex: 10, int: 16, per: 10, end: 14, cha: 10, sch1: "Хаос" }
+};
